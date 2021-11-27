12364cce22009b2c71ec87b9f9f22359dabf4ffb
trieuvanson <Vodanhserver@gmail.com>
2021-11-27 22:17:37 +0700

    test

--- src/components/mainpages/reports/Reports-Orders.jsx
+++ src/components/mainpages/reports/Reports-Orders.jsx
@@ -6,7 +6,7 @@ function ReportsOrders() {
     const state = useContext(GlobalState)
     const [orderRevenue] = state.reportsApi.orderRevenue
     const action  = state.reportsApi.orderAction
-
+/*
     const getQuantity = [
         orderRevenue.filter(item => item.month === 1)[0]?.quantity || 0,
         orderRevenue.filter(item => item.month === 2)[0]?.quantity || 0,
@@ -35,11 +35,11 @@ function ReportsOrders() {
         orderRevenue.filter(item => item.month === 10)[0]?.total || 0,
         orderRevenue.filter(item => item.month === 11)[0]?.total || 0,
         orderRevenue.filter(item => item.month === 12)[0]?.total || 0
-    ]
+    ]*/
 
     const year = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
 
-    const options = {
+    /*const options = {
         series: [{
             name: 'Số lượng',
             type: 'column',
@@ -67,6 +67,97 @@ function ReportsOrders() {
         },
         xaxis: {
             categories: ["Tháng 1","Tháng 2","Tháng 3","Tháng 4","Tháng 5","Tháng 6","Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"]
+        },
+        yaxis: [
+            {
+                axisTicks: {
+                    show: true,
+                },
+                axisBorder: {
+                    show: true,
+                    color: '#008FFB'
+                },
+                labels: {
+                    style: {
+                        colors: '#008FFB',
+                    }
+                },
+                title: {
+                    text: "Số lượng",
+                    style: {
+                        color: '#008FFB',
+                    }
+                },
+                tooltip: {
+                    enabled: true
+                }
+            },
+            {
+                seriesName: 'Income',
+                opposite: true,
+                axisTicks: {
+                    show: true,
+                },
+                axisBorder: {
+                    show: true,
+                    color: '#00E396'
+                },
+                labels: {
+                    style: {
+                        colors: '#00E396',
+                    }
+                },
+                title: {
+                    text: "Doanh thu",
+                    style: {
+                        color: '#26e7a6',
+                    }
+                },
+
+            },
+        ],
+        tooltip: {
+            fixed: {
+                enabled: true,
+                position: 'topLeft', // topRight, topLeft, bottomRight, bottomLeft
+                offsetY: 30,
+                offsetX: 60
+            },
+        },
+        legend: {
+            horizontalAlign: 'left',
+            offsetX: 40
+        }
+    };*/
+
+    const options = {
+        series: [{
+            name: 'Số lượng',
+            type: 'column',
+            data: orderRevenue.map(item => item.quantity)
+        }, {
+            name: 'Doanh thu',
+            type: 'column',
+            data: orderRevenue.map(item => item.total)
+        }],
+        chart: {
+            height: 350,
+            type: 'line',
+            stacked: false
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            width: [1, 1, 4]
+        },
+        title: {
+            text: 'Đơn hàng',
+            align: 'left',
+            offsetX: 110
+        },
+        xaxis: {
+            categories: orderRevenue.map(item => "Tháng " + item.month)
         },
         yaxis: [
             {
